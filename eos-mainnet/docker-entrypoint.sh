8c6a35e40923044b8deefedf3b9c028df02689d5
chakannom <genteelson@gmail.com>
2020-11-04 11:11:59 +0000

    eos-mainnet

--- eos-mainnet/docker-entrypoint.sh
+++ eos-mainnet/docker-entrypoint.sh
@@ -5,13 +5,15 @@ EOSIO_HOME=/home/eosio
 # setup term handler
 # SIGTERM-handler
 term_handler() {
-  ## histroy-tools
-  ### fill-pg
-  ${EOSIO_HOME}/history-tools/bin/fill-pg_stop.sh
-  sleep 1
-  ### postgresql
-  service postgresql stop
-  sleep 1
+  ## history-tools
+  if [[ ${ENABLE_HISTORY_TOOLS} == true ]]; then
+    ### fill-pg
+    ${EOSIO_HOME}/history-tools/bin/fill-pg_stop.sh
+    sleep 1
+    ### postgresql
+    service postgresql stop
+    sleep 1
+  fi
   ## node
   ${EOSIO_HOME}/node/bin/nodeos_stop.sh
   sleep 1
@@ -24,19 +26,21 @@ trap 'kill ${!}; term_handler' SIGTERM
 ## node
 ${EOSIO_HOME}/node/bin/nodeos_start.sh
 sleep 1
-## histroy-tool
-### posstgresql
-service postgresql start
-sleep 1
-### fill-pg
-STR_EXISTS=$(PGPASSWORD=eospsql psql -tA -h 127.0.0.1 -p 5432 -U eospsql -w -c "SELECT  EXISTS (SELECT FROM pg_catalog.pg_tables WHERE schemaname = 'chain' AND tablename = 'account')")
+## history-tools
+if [[ ${ENABLE_HISTORY_TOOLS} == true ]]; then
+  ### posstgresql
+  service postgresql start
+  sleep 1
+  ### fill-pg
+  STR_EXISTS=$(PGPASSWORD=eospsql psql -tA -h 127.0.0.1 -p 5432 -U eospsql -w -c "SELECT  EXISTS (SELECT FROM pg_catalog.pg_tables WHERE schemaname = 'chain' AND tablename = 'account')")
 
-if [[ ${STR_EXISTS} == 'f' ]]; then
-  ${EOSIO_HOME}/history-tools/bin/fill-pg_start.sh init
-else
-  ${EOSIO_HOME}/history-tools/bin/fill-pg_start.sh
+  if [[ ${STR_EXISTS} == 'f' ]]; then
+    ${EOSIO_HOME}/history-tools/bin/fill-pg_start.sh init
+  else
+    ${EOSIO_HOME}/history-tools/bin/fill-pg_start.sh
+  fi
+  sleep 1
 fi
-sleep 1
 
 # wait forever
 while true
